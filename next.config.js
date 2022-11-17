76127f2ca53426563130688636057a7fd297b73b
CelyQ <paul.opmann@gmail.com>
2022-11-17 18:29:24 +0900

    removed locales

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en-US', 'ko-KR'],
-    defaultLocale: 'en-US'
-  },
-  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
